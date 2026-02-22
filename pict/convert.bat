36cb91cd83e0c1688a7fc24e210e5d33cc69cf87
Ilia Motornyi <ilia.motornyi@jetbrains.com>
2026-02-22 03:16:05 +0200

    Pictures

---
+++ pict/convert.bat
@@ -0,0 +1,3 @@
+for %%f in (svg\*.svg) do (
+	"C:\Program Files\Inkscape\bin\inkscape.exe" "%%f" --export-type=png --export-filename="svg_png\%%~nf.png" --export-width=80 --export-height=80 --export-background-opacity=1.0 --export-background=#FF7F00
+)
